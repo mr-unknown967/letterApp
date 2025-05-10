8688a954764036c66a3657d5d61c242db2710fde
mr-unknown967 <mrukknown967@gmail.com>
2025-05-10 16:12:25 +0530

    Fix TypeScript error in cache implementation

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -33,7 +33,9 @@ class EnhancedCache {
     // If cache is full, remove oldest entries
     if (this.cache.size >= this.maxSize) {
       const oldestKey = this.cache.keys().next().value;
-      this.cache.delete(oldestKey);
+      if (oldestKey !== undefined) {
+        this.cache.delete(oldestKey);
+      }
     }
 
     this.cache.set(key, {
